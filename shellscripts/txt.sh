20e161fe259dab5d96aa57f602ecf4c84f423252
Lars Ole Simonsen <los@c3a.dk>
2015-04-09 12:43:57 +0200

    Missing changes for recently added scripts

--- shellscripts/txt.sh
+++ shellscripts/txt.sh
@@ -2,24 +2,14 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-SUBJECTEXTENSION="coffee"
+SUBJECTEXTENSION="txt"
 SUBJECTPATH="./app/coffee/controllers/margeController.${SUBJECTEXTENSION}"
 OTHERBRANCH="develop"
 
-OUTPUTFOLDER="corpus/test"
-ORIGINAL="${OUTPUTFOLDER}/original.${SUBJECTEXTENSION}"
-FUTURE1="${OUTPUTFOLDER}/future1.${SUBJECTEXTENSION}"
-FUTURE2="${OUTPUTFOLDER}/future2.${SUBJECTEXTENSION}"
-
-
-COMMIT1="$(git rev-parse ${OTHERBRANCH})"
-COMMIT2="HEAD"
-ANCESTORCOMMIT="$(git merge-base ${COMMIT1} ${COMMIT2})"
-
-mkdir -p "$OUTPUTFOLDER"
-git show "${ANCESTORCOMMIT}:${SUBJECTPATH}" > "$ORIGINAL"
-git show "${OTHERBRANCH}:${SUBJECTPATH}" > "$FUTURE1"
-cat "$SUBJECTPATH" > "$FUTURE2"
+OUTPUTFOLDER="corpus/txt"
+ORIGINAL="${OUTPUTFOLDER}/original.txt"
+FUTURE1="${OUTPUTFOLDER}/future1.txt"
+FUTURE2="${OUTPUTFOLDER}/future2.txt"
 
 ATOMPATH="${SCRIPT_DIR}/../binaries/Atom.app/Contents/MacOS/Atom"
 
